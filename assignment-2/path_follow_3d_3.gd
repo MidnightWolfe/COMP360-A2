e0a5c8342932e27412d3f49c8a0997b9a69a850d
StudentInTraining0005 <jannine.gemmell@student.ufv.ca>
2024-10-25 14:17:15 -0700

    Update path_follow_3d_3.gd

--- assignment-2/path_follow_3d_3.gd
+++ assignment-2/path_follow_3d_3.gd
@@ -1,15 +1,15 @@
 extends PathFollow3D
-var first =0
+
+var first = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if first == 0:
 		first = 1
-		progress = .1
-	progress += delta*1
+		progress = 0.1
+	progress += delta
 	pass
