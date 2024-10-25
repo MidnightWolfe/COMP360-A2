f01723bffd9ce8ccf2d893dfb1d9ad390a841378
StudentInTraining0005 <jannine.gemmell@student.ufv.ca>
2024-10-25 14:17:49 -0700

    Update spline_path.gd

--- assignment-2/spline_path.gd
+++ assignment-2/spline_path.gd
@@ -1,8 +1,8 @@
-extends Path3D
 ##NOTE: Matrix class is from our lectures / labs and is only slightly changed
+extends Path3D
 
 ###Global variables
-var pointsCurve = _hilbertPoints(3, 200) #The points of the Hilbert Curve
+var pointsCurve = _HilbertPoints(3, 200) #The points of the Hilbert Curve
 var splinePoints = [] #Global variable for the points for the cardinal / catmull-rom spline
 
 ##Static information for the spline / spline point creation
@@ -12,11 +12,11 @@ var splineScale = 0.5 #Scale of the spline, 0.5 makes this a catmull-rom, which 
 ##Static variables for spline image generation
 var splineImage = Image.create(200, 200, false, Image.FORMAT_RGBA8) #Used to create the image of the spline
 
-##Positioning variables for the image as well as the path, but SINCE the the path and the image have different origins you;ll have to also tune the path3D in create3Dpathinworld func
+##Positioning variables for the image as well as the path
 var pathXOffset = 15
 var pathYOffset = 1.8
 var pathZOffset = 10
-	
+
 ##The start and end points of the slice for the Hilbert Curve
 var start =  2 #start of the slice
 var end = 30 #End of the slice
@@ -29,17 +29,14 @@ func _ready() -> void:
 	points.insert(0, pointsCurve[start])
 	
 	##Calling the spline
-	catmullRom()
-	#print(pointsCurve) #Testing
-	#print(points) #Testing
-	#print(splinePoints) #Testing
+	_CatmullRom()
 	
 	##Create the 3D path now that the spline is made
-	createPath3DInWorld()
+	_CreatePath3DInWorld()
 	pass
 
 ##The function to get the points of the spline based on the projected points and the characteristics of the cardinal spline
-func cardinalSpline(projectedPoints):
+func _CardinalSpline(projectedPoints):
 	##Getting the points after they are altered by the characterisitcs of the cardinal spline
 	##and multiplying them by the projected points from the knots
 	for i in range(resolution):
@@ -66,26 +63,23 @@ func cardinalSpline(projectedPoints):
 		var multiplyResult = polynominalT.multiply(splineMatrix).multiply(projectedMatrix) #The new matrix
 		splinePoints.append(multiplyResult.get_value(0, 0)) #The new points after the multiplication
 	return splinePoints
-	
-	
-	
+
 ##Generating the cardinal spline using points from the hilbert curve
-func catmullRom():
+func _CatmullRom():
 	##The loop for getting each projected points of the spline (based on knots: k)
 	##This also allows for the curve in the spline, rather than right angles
 	for k in range(points.size()-3):
-		splinePoints = cardinalSpline(
+		splinePoints = _CardinalSpline(
 			[
 				[points[k]],
 				[points[k+1]],
 				[points[k+2]],
 				[points[k+3]]
-			
 			]
 		)
 		##Loop to draw / generate the line of the spline based on the points
 		for i in range(splinePoints.size()-1):
-			bresenhamLine(splinePoints[i].x, splinePoints[i].y, splinePoints[i+1].x, splinePoints[i+1].y )
+			_BresenhamLine(splinePoints[i].x, splinePoints[i].y, splinePoints[i+1].x, splinePoints[i+1].y )
 	
 	##Generating the 2D image version of the spline to be used for the 3D image path	
 	var splineTexture = ImageTexture.create_from_image(splineImage)
@@ -94,19 +88,15 @@ func catmullRom():
 	splineSprite.texture = splineTexture
 	
 	#create an in world image of the spline
-	createImageInWorld(splineSprite)
-	
+	_CreateImageInWorld(splineSprite)
 	pass
 
 ##Function to get the points from a Hilbert space-filling curve
-func _hilbertPoints(level : int, size : int) -> PackedVector2Array:
-	
+func _HilbertPoints(level : int, size : int) -> PackedVector2Array:
 	## Number of times the space is being divided
 	var divisions = pow(2, level)
-	
 	## Total number of points
 	var numOfPoints = divisions * divisions
-	
 	## This is the base case for a level 1 Hilbert curve
 	var basePoints = PackedVector2Array([
 		Vector2(0,1),
@@ -114,13 +104,11 @@ func _hilbertPoints(level : int, size : int) -> PackedVector2Array:
 		Vector2(1,0),
 		Vector2(1,1)
 	])
-	
 	## The array we will be adding points to
 	var pointsHilbert : PackedVector2Array
 	
 	## Loop over each point using it's index to determine its location
 	for i in int(numOfPoints):
-		
 		## Using bit masking to look at the first two bits to determine it's base case
 		var index = i & 3
 		var pt = basePoints[index]
@@ -136,13 +124,10 @@ func _hilbertPoints(level : int, size : int) -> PackedVector2Array:
 		## -------
 		## Above it how the bits correspond to what quadrant it lies in
 		for j in range(1, level):
-			
 			## Shift bits
 			i = i >> 2
-			
 			## Mask to look at the first two
 			index = i & 3
-			
 			## Length relative to level of curve
 			var length = pow(2,j)
 			
@@ -152,15 +137,12 @@ func _hilbertPoints(level : int, size : int) -> PackedVector2Array:
 				pt.x = length - 1 - pt.y
 				pt.y = temp
 				pt += Vector2(0, length)
-			
 			## Top left quadrant
 			else: if index == 1:
 				pass
-			
 			## Top right quadrant
 			else: if index == 2:
 				pt += Vector2(length, 0)
-			
 			## Bottom right quadrant
 			else: if index == 3:
 				var temp = pt.x
@@ -176,10 +158,8 @@ func _hilbertPoints(level : int, size : int) -> PackedVector2Array:
 		pointsHilbert.append(pt)
 	return pointsHilbert
 
-
-
 ##Based (though altered) on lecture / lab code generated by Gemini and the actual bresenham algorithm
-func bresenhamLine(funcX0, funcY0, funcX1, funcY1):
+func _BresenhamLine(funcX0, funcY0, funcX1, funcY1):
 	var x1 = floor(funcX0)
 	var x2 = floor(funcX1)
 	var y1 = floor(funcY0)
@@ -206,11 +186,9 @@ func bresenhamLine(funcX0, funcY0, funcX1, funcY1):
 		if error2 <= differenceX:
 			error += differenceX
 			y1 += slopeY
-			
+
 ##This takes the image made by catMullRom and turns it into a Sprite 3D then positions it so it will be visible in the world
-func createImageInWorld(image: Sprite2D):
-	
-	
+func _CreateImageInWorld(image: Sprite2D):
 	#Find pathimage and add image to it is the overarching goal here
 	#We could totally generate this on the fly instead of having a pre done node
 	var path_image_node = get_node("../PathImage")
@@ -227,27 +205,20 @@ func createImageInWorld(image: Sprite2D):
 	var new3DSprite = Sprite3D.new()
 	#Set up the 3D sprite with the new texture
 	new3DSprite.texture = textureFromSprite2D
-
 	
 	#Rotate the image. Thanks to the info from today's class to make that easy
 	var th = PI/2
 	new3DSprite.transform.basis = Basis.IDENTITY.rotated(Vector3(-1, 0, 0), th)
-
 	#Add the 3D sprite to the Scene
 	path_image_node.add_child(new3DSprite)
-	
 	#Set the 3DSprite's default position based on the offsets (Global)
 	new3DSprite.transform.origin = Vector3(pathXOffset,pathYOffset,pathZOffset)
-	
 	#Scale if wanted
 	new3DSprite.scale = Vector3(12.0,8.0,1)
-	
-	
 	pass
-	
-	
+
 ##Create a path3D from the points used to generate the image, these points are taken directly from the splinePoints[]. I found this easier than tracing it as my image only exists in runtime
-func createPath3DInWorld():
+func _CreatePath3DInWorld():
 	#The points are way too spread apart for the size of our landscape this will shrink it down and make the path3D be scalable
 	var shrinkFactorX = 8.2
 	var shrinkFactorZ = 12.4
@@ -261,7 +232,7 @@ func createPath3DInWorld():
 	
 	for point in splinePoints:
 		#Convert vector2 to vector 3 while also adjusting the positioning of the points to fit the image we generated
-		#NOTE This is a manual step, you must adjust the values that the offsets are to move the path3D moving the image has no interaction with the path
+		##NOTE This is a manual step, you must adjust the values that the offsets are to move the path3D moving the image has no interaction with the path
 		var pointX = point.x/shrinkFactorX + path3DXOffset
 		@warning_ignore("integer_division")
 		#Y becomes Z due to rotation of the plane
@@ -270,14 +241,11 @@ func createPath3DInWorld():
 		#Add the now modified point to the curve
 		self.curve.add_point(pointIn3DSpace)
 	#Show the path with objects for testing purposes
-	debugShowPath(self.curve)
-	
-	
+	_DebugShowPath(self.curve)
 	pass
-	
-	
-	##Shows the location of the actual curve 3D which is normally transparent
-func debugShowPath(splineCurve: Curve3D):
+
+##Shows the location of the actual curve 3D which is normally transparent
+func _DebugShowPath(splineCurve: Curve3D):
 	#for each point in curve
 	for i in range(splineCurve.get_point_count()):
 		#Get the position of the point
